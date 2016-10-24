39094dedff956d5dd5b7160a396de037a2f8ab0c
momokos2 <irenelyh212@gmail.com>
2016-10-24 14:36:51 -0700

    (1)finished product-detail.html (2)finished comments-page.html (2)added write-review.html

--- js/index.js
+++ js/index.js
@@ -9,3 +9,35 @@ $(document).ready(function() {
     });
     $('section').hide();
 });
+
+
+$(function() {
+  
+  var photoGallery = function($photoGalleryElement) {
+    var $photoGallery = $photoGalleryElement,
+        $photoLinks = $photoGallery.find('a[href*="#photo"]'),
+        $photos = $photoGallery.find('img[id*="photo"]'),
+        activeClass = 'active';
+    
+    function init() {
+      $photoGallery.addClass('enabled');
+         
+      $photoGallery.on({
+        click: function(e) {
+          e.preventDefault();
+          var $photoTarget = $(this).attr('href');
+          $photos.removeClass(activeClass);
+          $photoGallery.find($photoTarget).addClass(activeClass);
+        }
+      }, 'a[href*="#photo"]')
+    }
+    
+    init();
+  }
+  
+  $.each($('.photo-gallery'), function() {
+    // Try commenting out this line below to see no-js functionality!
+    var gallery = new photoGallery( $(this) );
+  });
+  
+});
