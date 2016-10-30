c24f5b6b592d7a55c40350bdb1de135ee4da3d1a
momokos2 <irenelyh212@gmail.com>
2016-10-30 15:44:14 -0700

    (1)changed productlist.html to product-list.html (2)added new responsive nav

--- js/index.js
+++ js/index.js
@@ -44,6 +44,23 @@ $(function() {
 });
 
 
+$(document).ready(function(){
+
+  (function($) {
+
+    $('#header-icon').click(function(e){
+      e.preventDefault();
+      $('body').toggleClass('with--sidebar');
+    });
+    
+    $('#site-cache').click(function(e){
+      $('body').removeClass('with--sidebar');
+    });
+
+  })(jQuery);
+
+});
+
 
 
 
